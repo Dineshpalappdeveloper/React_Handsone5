57ddcd82bc3d05f28a6c90daedea64f56bb6b4a2
Dineshpalappdeveloper <dineshkr748199@gmail.com>
2023-07-31 12:44:44 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 
+// import { useState } from 'react';
 import './App.css';
-
+import PureCompo from './PureCompo';
+import { useState } from 'react'
 function App() {
+  // const [count,setcount]=useState(10);
   return (
-    <div className="App">
+    <div className="App" >
       <div className='left'>
-      <h1>Pure Component</h1>
+      <PureCompo></PureCompo>
       </div>
       <div className='right'>
       <h1>High Order Component</h1>
+      <h3 style={{color:"red"}}>it component as argument and always return a new component it means
+       take input as component and give component as output
+       </h3>
+       <HocRed compo={Counter} />
+       <HocGreen compo={Counter} />
+  
 </div>
  
     </div>
   );
+}
+ function HocRed(props){
+   return <h2 style={{backgroundColor:"red",width:"180px" }}> <props.compo/></h2>
+ }
+ function HocGreen(props){
+  return <h2 style={{backgroundColor:"green",width:"180px" }}> <props.compo/></h2>
+}
+
+function Counter(){
+  const[count,setcount]=useState(0);
+  return <div>
+      <h3>{count}</h3>
+      <button  onClick={()=>setcount(count+1)}>Increase value</button>
+  </div>
 }
 
 export default App;
